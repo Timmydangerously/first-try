0da6a77c3692bb723d7677a4267b041ffd4b230a
timmydangerously <caspersim62@gmail.com>
2024-11-23 07:39:08 -0800

    still search bar

--- src/components/Navbar-components/Searchbar.jsx
+++ src/components/Navbar-components/Searchbar.jsx
@@ -1,28 +1,44 @@
 "use client"
 
 import React from 'react'
-import { Searchvideos } from '@/utility/fetchdata'
+import { SearchVideos } from '@/utility/fetchdata'
 import { useState } from 'react'
 
 const Searchbar = () => {
-  const [count, setCount ] = useState(0)
-  function update() {
-    setCount(count+1)
+  //const [count, setCount ] = useState(50)
+ // function update() {
+   // setCount(count-1)
+  //}
+  const [ loading, setLoading ] = useState(false)
+  const [ result, setResult ] = useState([])
+  const [ query, setQuery ] = useState("not recognised")
+
+  const handleSearch = async (e) =>{
+    e.preventDefault();
+    setLoading(true)
+    const data = await SearchVideos(`search/?query=${query}`)
+    console.log(data)
+    setResult(data)
+    setLoading(false)
   }
   
   
     return (
     <div>
-        <form>
-        <input type="text" placeholder='search here...' />
+        <form onSubmit={handleSearch}>
+        <input type="text" placeholder='search here...'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}/>
 <button>
     Search
 </button>
         </form>
-        <h1>{count}</h1>
-        <button onClick={update}>update count</button>
+       {loading ? <p> waiting....</p> : null}
     </div>
-  )
-}
+  )}
+
+
+
+
 
 export default Searchbar
--- src/utility/fetchdata.js
+++ src/utility/fetchdata.js
@@ -1,14 +1,14 @@
 const base_url =  'https://youtube-v2.p.rapidapi.com'
 
 
-export const Searchvideos = (query) =>{
-    const response = fetch(`${base_url}/${query}`, {
+export const SearchVideos = async (param) =>{
+    const response = await fetch(`${base_url}/${param}`, {
         method: "GET",
         headers: {
             'x-rapidapi-key': 'bb65b5652fmsh0f7f08d5012eca5p155fa6jsn4eb15004d303',
 		'x-rapidapi-host': 'youtube-v2.p.rapidapi.com'
         }
     })
-    const result = response.json();
+    const result = await response.json();
     return result;
 }
